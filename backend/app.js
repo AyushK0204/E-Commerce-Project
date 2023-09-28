d9ccb7c3c07b77cb55954334ddbb16ee0f33d0bb
AyushK0204 <ayu.k.210402@gmail.com>
2023-09-28 19:03:57 +0530

    add pre i

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 const errorMiddleware = require("./middleware/error");
+const path = require("path");
 
 //Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -27,6 +28,12 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // Middleware for Errors
 
