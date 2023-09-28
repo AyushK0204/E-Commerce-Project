6c5dcba9e8350e29f6f350d887b89f7e1dbdec1a
AyushK0204 <ayu.k.210402@gmail.com>
2023-09-28 18:46:41 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 const errorMiddleware = require("./middleware/error");
-const path = require("path");
 
 //Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -28,12 +27,6 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
 
 // Middleware for Errors
 
